8407c335ea961f9968b49962b3afc7deccf518e4
varun-mach <vmaching@iu.edu>
2018-02-09 16:46:18 -0500

    comment added in author

--- src/main/java/com/example/demo/model/Author.java
+++ src/main/java/com/example/demo/model/Author.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+//Class for author
 @Entity
 public class Author {
 
